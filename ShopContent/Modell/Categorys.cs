44c906741b14b9d2dffa9d06fd6615ad74f426a1
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 10:24:22 +0500

    Разработанные модели данных. Белослудцева

---
+++ ShopContent/Modell/Categorys.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Modell
+{
+    public class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+           
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ShopContent/Modell/Items.cs
@@ -0,0 +1,76 @@
+﻿using ShopContent.Modell;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int id;
+       
+        public int Id
+        {
+           
+            get { return id; }
+          
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            
+            get { return price; }
+           
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        private Categorys category;
+        public Categorys Category
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Category"); 
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
