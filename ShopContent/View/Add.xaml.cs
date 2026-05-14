3d9d84203029d8e4cb263170fb7c8b6f15ab29bb
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 10:37:20 +0500

    Созданная страница добавления и изменения. Белослудцева

---
+++ ShopContent/View/Add.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ShopContent.Modell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopContent.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object Context)
+        {
+            InitializeComponent();
+            DataContext = new
+            {
+                item = Context,
+                categorys = new ViewModel.VMCategorys()
+            };
+        }
+    }
+}
