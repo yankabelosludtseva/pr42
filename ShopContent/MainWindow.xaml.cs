e45d7ebfa9ebbce2a00f52559cd5ff0c4d245085
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 09:49:31 +0500

    Разработанное главное окно. Белослудцева

--- ShopContent/MainWindow.xaml.cs
+++ ShopContent/MainWindow.xaml.cs
@@ -16,9 +16,16 @@ namespace ShopContent
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public View.Main Main = new View.Main();
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(Main);
         }
+
+        private void OpenIndex(object sender, MouseButtonEventArgs e) =>
+            frame.Navigate(Main);
     }
 }
