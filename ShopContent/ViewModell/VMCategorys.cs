6690bc113ea34560d418b1f0f8e6073410a7c6a4
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 10:32:09 +0500

    Разработанные модели представления. Белослудцева

---
+++ ShopContent/ViewModell/VMCategorys.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.ViewModel
+{
+    public class VMCategorys : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.CategorysContext> Categorys { get; set; }
+
+        public VMCategorys() =>
+            Categorys = Context.CategorysContext.AllCategorys();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ShopContent/ViewModell/VMItems.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.ViewModel
+{
+    public class VMItems : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.ItemsContext> Items { get; set; }
+
+        public Classes.RelayCommand NewItem
+        {
+            get
+            {
+                return new Classes.RelayCommand(obj => {
+                    Context.ItemsContext newModell = new Context.ItemsContext(true);
+                    Items.Add(newModell);
+                    MainWindow.init.frame.Navigate(new View.Add(newModell));
+                });
+            }
+        }
+
+        public VMItems() =>
+            Items = Context.ItemsContext.AllItems();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
